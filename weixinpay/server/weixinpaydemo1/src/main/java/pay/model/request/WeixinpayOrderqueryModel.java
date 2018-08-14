3b3a3e4c0d23c6478f2bac7e78f5c7fcc88674c1
wangyu19810820 <gadfly0036_cn@sina.com>
2018-08-14 11:57:53 +0800

    测试用例调整

--- weixinpay/server/weixinpaydemo1/src/main/java/pay/model/request/WeixinpayOrderqueryModel.java
+++ weixinpay/server/weixinpaydemo1/src/main/java/pay/model/request/WeixinpayOrderqueryModel.java
@@ -2,26 +2,26 @@ package pay.model.request;
 
 public class WeixinpayOrderqueryModel extends WeixinpanServerModel {
 
-//    // 商户订单号
-//    private String out_trade_no;
+    // 商户订单号
+    private String out_trade_no;
 
-    // 微信订单号, 与out_trade_no二选一，微信文档推荐用transaction_id
-    private String transaction_id;
+//    // 微信订单号, 与out_trade_no二选一，微信文档推荐用transaction_id
+//    private String transaction_id;
 
-//    public String getOut_trade_no() {
-//        return out_trade_no;
+    public String getOut_trade_no() {
+        return out_trade_no;
+    }
+
+    public void setOut_trade_no(String out_trade_no) {
+        this.out_trade_no = out_trade_no;
+    }
+
+
+//    public String getTransaction_id() {
+//        return transaction_id;
 //    }
 //
-//    public void setOut_trade_no(String out_trade_no) {
-//        this.out_trade_no = out_trade_no;
+//    public void setTransaction_id(String transaction_id) {
+//        this.transaction_id = transaction_id;
 //    }
-
-
-    public String getTransaction_id() {
-        return transaction_id;
-    }
-
-    public void setTransaction_id(String transaction_id) {
-        this.transaction_id = transaction_id;
-    }
 }
--- weixinpay/server/weixinpaydemo1/src/main/test/http/WeixinpayInterfaceUtilTest.java
+++ weixinpay/server/weixinpaydemo1/src/main/test/http/WeixinpayInterfaceUtilTest.java
@@ -17,13 +17,10 @@ public class WeixinpayInterfaceUtilTest {
         model.setAppid("wx49680a1b177c1fd7");
         model.setBody("bbb");
         model.setNotify_url("http://abc.com");
-        model.setOut_trade_no("201808141016aaa");
+        model.setOut_trade_no("201808141016bbb");
         model.setSpbill_create_ip("122.12.11.11");
         model.setTotal_fee("1");
         model.setTrade_type("APP");
-        model.setKey("0757cc3ee0d74ba9aae6e3d9d85a7398");
-        model.setMch_id("1509949201");
-        model.setNonce_str("abc");
 
         WeixinpayUnifiedorderResponse result = WeixinpayInterfaceUtil.unifiedorder(model);
         System.out.println(result);
@@ -32,13 +29,7 @@ public class WeixinpayInterfaceUtilTest {
     @Test
     public void orderqueryTest() throws Exception {
         WeixinpayOrderqueryModel model = new WeixinpayOrderqueryModel();
-        model.setAppid("wx49680a1b177c1fd7");
-        model.setMch_id("1494287092");
-        model.setTransaction_id("1");
-        model.setKey("81abddd648f64ae78d2f08696874262d");
-        model.setMch_id("1494287092");
-        model.setNonce_str("abc");
-
+        model.setOut_trade_no("201808141016aaa");
         WeixinpayOrderqueryResponse result = WeixinpayInterfaceUtil.unifiedorder(model);
         System.out.println(result);
     }
@@ -46,13 +37,7 @@ public class WeixinpayInterfaceUtilTest {
     @Test
     public void closeorderTest() throws Exception {
         WeixinpayCloseorderModel model = new WeixinpayCloseorderModel();
-        model.setAppid("wx49680a1b177c1fd7");
-        model.setMch_id("1494287092");
-        model.setOut_trade_no("1");
-        model.setKey("81abddd648f64ae78d2f08696874262d");
-        model.setMch_id("1494287092");
-        model.setNonce_str("abc");
-
+        model.setOut_trade_no("201808141016aaa");
         WeixinpayCloseorderResponse result = WeixinpayInterfaceUtil.closeorder(model);
         System.out.println(result);
     }
@@ -60,12 +45,7 @@ public class WeixinpayInterfaceUtilTest {
     @Test
     public void refundTest() throws Exception {
         WeixinpayRefundModel model = new WeixinpayRefundModel();
-        model.setAppid("wx49680a1b177c1fd7");
-        model.setMch_id("1494287092");
-        model.setOut_trade_no("1");
-        model.setKey("81abddd648f64ae78d2f08696874262d");
-        model.setMch_id("1494287092");
-        model.setNonce_str("abc");
+        model.setOut_trade_no("201808141016aaa");
         model.setTotal_fee("1");
         model.setRefund_fee("1");
 
@@ -76,13 +56,7 @@ public class WeixinpayInterfaceUtilTest {
     @Test
     public void refundqueryTest() throws Exception {
         WeixinpayRefundqueryModel model = new WeixinpayRefundqueryModel();
-        model.setAppid("wx49680a1b177c1fd7");
-        model.setMch_id("1494287092");
-        model.setKey("81abddd648f64ae78d2f08696874262d");
-        model.setMch_id("1494287092");
-        model.setNonce_str("abc");
-        model.setOut_trade_no("1");
-
+        model.setOut_trade_no("201808141016aaa");
         WeixinpayRefundResponse result = WeixinpayInterfaceUtil.refundquery(model);
         System.out.println(result);
     }
